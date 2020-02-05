b61bcebc41725449c25e051b081a0c237dd23138
rileyhillis97 <54870567+rileyhillis97@users.noreply.github.com>
2020-02-05 08:45:05 -0600

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,12 @@
+def main():
+    more = 'y'
+    while more == 'y' or more == 'Y'
+     print("\n\n\t\tCalculate the real cost of a meal, where ")
+     print("\n\n\t\t tax is 7% and be nice to add 18% tip
+
+
+
+
+
+main()
+        
