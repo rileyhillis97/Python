3685943481a01b89507e6096da7c4616b7b66fa5
rileyhillis97 <54870567+rileyhillis97@users.noreply.github.com>
2020-02-06 07:59:43 -0600

    Add files via upload

--- lab2.py
+++ lab2.py
@@ -1,8 +1,23 @@
 def main():
     more = 'y'
-    while more == 'y' or more == 'Y'
+    while more == 'y' or more == 'Y':
      print("\n\n\t\tCalculate the real cost of a meal, where ")
-     print("\n\n\t\t tax is 7% and be nice to add 18% tip
+     print("\t\t tax is 7% and be nice to add 18% tip ")
+     cost = float(input("\n\t\t\tMeal Cost: "))
+     
+     print("\n\t\t\t Meal : $ ", cost)
+     tax = (cost * .07)
+     print("\n\t\t\t  tax : $ ", format(cost * .07, '.2f'))
+     tip = (cost * .18)
+     print("\n\t\t\t  tip : $ ", format(cost * .18, '.2f'))
+     print("\n\t\t\tTotal : $ ", format(tip + cost + tax, '.2f'))
+                                    
+        
+   
+
+
+
+     more = input ("\n\t\tDo more (Y/N) ? ")
 
 
 
